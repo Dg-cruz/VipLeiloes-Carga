64f91adf10652bc30fe83324985dd5f38fa033e8
Diego Cruz <diegoogc@gmail.com>
2025-08-12 00:48:58 -0300

    Finalizado testes detalhes do anuncio

---
+++ config/detalhes_anuncio.js
@@ -0,0 +1,5 @@
+// URLs alvo
+export const BASE_URL = 'https://hom-frontend.vipleiloes.com.br';
+export const ATUALIZACAO_URL = BASE_URL + '/evento/atualizacao?anuncioId=';
+export const headers = { 'Content-Type': 'application/json' };
+export const ANUNCIO_ID = '6eb631cf-efde-42dc-b42e-b32900cb1d8f'; // ID do anúncio para teste
