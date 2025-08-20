abc895524ed76f89eb984f222aa782c85f61251d
Diego Cruz <diegoogc@gmail.com>
2025-08-20 02:41:35 -0300

    Finalizando os testes

--- config/staging.js
+++ config/staging.js
@@ -1,2 +1,3 @@
 export const baseURL = 'https://hom-frontend.vipleiloes.com.br/';
+export const buscaURL = 'https://hom-frontend.vipleiloes.com.br/pesquisa/index?Filtro.LocalEstadoId=&Filtro.LocalCidade=&Filtro.Texto=';
 export const headers = { 'Content-Type': 'application/json' };
--- scripts/home/buscaAvancada.js
+++ scripts/home/buscaAvancada.js
@@ -1,16 +1,16 @@
 // Aumenta gradualmente o número de usuários para descobrir o ponto de quebra.
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { baseURL } from '../config/staging.js'; // importa a URL base da pasta config
+import { buscaURL } from '../../config/staging.js'; // importa a URL base da pasta config
 
 export let options = {
   stages: [
-  { duration: '2m', target: 100 },
-  { duration: '3m', target: 500 },
-  { duration: '2m', target: 1000 },
-  { duration: '3m', target: 1500 },
-  { duration: '2m', target: 0 }
-],
+      { duration: '2m', target: 1000 },
+      { duration: '3m', target: 2000 },
+      { duration: '2m', target: 2500 },
+      { duration: '3m', target: 2500 },
+      { duration: '2m', target: 0 }
+    ],
   thresholds: {
     http_req_duration: ['p(95)<1000'],
   },
@@ -18,7 +18,7 @@ export let options = {
 };
 
 export default function () {
-  const res = http.get(baseURL);
+  const res = http.get(buscaURL);
 
   check(res, {
     'status é 200': (r) => r.status === 200,
